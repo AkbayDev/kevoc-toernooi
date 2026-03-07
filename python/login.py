3d64f18cc23aefecb7c45b08e905bc0c474f66fa
Grain <fadlaouiouadie@gmail.com>
2026-03-07 18:06:44 +0100

    new update

--- python/login.py
+++ python/login.py
@@ -15,7 +15,7 @@ from routes.vrijwilligers import vrijwilligers_bp
 from routes.rooster import rooster_bp
 
 # Importeer de Blueprint vanuit dash.py
-from dash import dash_bp
+from routes.dash import dash_bp
 
 app = Flask(__name__)
 CORS(app)
